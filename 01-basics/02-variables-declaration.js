c6d630fc4b60629527f0ee1e58dd1161d7e311a4
AlexKomanov <akomanov88@gmail.com>
2024-07-20 14:27:59 +0300

    Adding lesson 3

---
+++ 01-basics/02-variables-declaration.js
@@ -0,0 +1,82 @@
+// Without variables
+// console.log("Lorem Ipsum is simply dummy text of the printing!")
+// console.log("Lorem Ipsum is simply dummy text of the printing!")
+// console.log("Lorem Ipsum is simply dummy text of the printing!")
+
+// With variables
+// const text = "Lorem Ipsum is simply dummy text of the printing!!!"
+// console.log(text)
+// console.log(text)
+// console.log(text)
+
+// Java example
+// String name = "Alex";
+// int age = 36;
+
+// var keyword
+// var a = 5
+// console.log(a)
+// var a2;
+// a2 = 200
+// console.log(a2)
+// a2 = 500
+// console.log(a2)
+
+// let keyword - since ES2015 (ES6)
+// let b = 10
+// console.log(b)
+// let b2;
+// b2 = 25
+// console.log(b2)
+// b2 = 1000
+// console.log(b2)
+
+// const keyword - since ES2015 (ES6)
+// const c = 67
+// console.log(c)
+
+// const c2;
+// c2 = 200
+// console.log(c2)
+
+// Dynamic types
+// let income = 5000
+// console.log(income)
+// console.log(income - 1000)
+// income = "Alex"
+// console.log(income)
+// console.log(income + 1000)
+
+// let vs var
+// var age = 5
+// console.log(age)
+// var age = 56
+// console.log(age)
+
+// let age2 = 5
+// console.log(age2)
+// let age2 = 56
+// console.log(age2)
+
+// const vs let
+
+// let item = "item"
+// console.log(item)
+// item = "item 2"
+// console.log(item)
+
+// const item2 = "item"
+// console.log(item2)
+// item2 = "item2"
+// console.log(item2)
+
+// Declare variables via const
+// const text = element.getText() // Exapmle of reading text from element
+// expect(text).toBe("text") // Exapmle of validation
+
+// without keywords
+// globalA = "Name"
+// console.log(globalA)
+// console.log(globalThis.globalA)
+// let globalA = 7
+
--- 01-basics/demo.js
+++
@@ -1,7 +0,0 @@
-a = 5
-console.log(a)
-
-a = "Name"
-console.log(globalThis.a)
-
-globalThis.a
