7ae6783c9c388cc4e0bf1af999f681222ea55d86
AlexKomanov <akomanov88@gmail.com>
2024-07-27 06:29:54 +0300

    adding lesson 04

---
+++ 01-basics/03-variables-names.js
@@ -0,0 +1,48 @@
+
+/* Variable can start with: letter | $ | _  */
+// let first = "Alex"
+// let $name = "Alexander"
+// let _name = "Sasha"
+// let _full_$name_36 = "Alex Komanov"
+
+// console.log(first)
+// console.log($name)
+// console.log(_name)
+// console.log(_full_$name_36)
+
+
+/* Variable can't start with: nubmer | include spaces | include -   */
+// let 1first = "Alex"
+// let first name = "Alexander"
+// let first-second = "Sasha"
+
+/* JavaScript is case sensitive   */
+// let first = "Alex"
+// // let first = "Alex"
+// let First = "Alexander"
+// console.log(first)
+// console.log(First)
+// console.log(first)
+
+/* In most cases JavaScript uses camelCase notation. But also UPPER_CASE notation  */
+
+// const myFullName = "Alexander Komanov"
+// const MY_ID = 345677
+// const ITEMS_URL = "/api/items"
+// console.log("www.store.com" + ITEMS_URL)
+
+/* Reserved words can be used as variables names  */
+
+// const new = "My new Song";
+// const default = 56
+// const newSong = "My new Song";
+// const defaulAge = 56
+// const final = 45645
+// console.log(final)
+
+const Final_Countdown = 4564568
+const _privateName = "Alex"
+
+
+
+
