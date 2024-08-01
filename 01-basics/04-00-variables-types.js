b7e7eef25f25dc9645cd5dc0b51d4f17a7a6a625
AlexKomanov <akomanov88@gmail.com>
2024-08-01 13:15:12 +0300

    Adding variables types lesson

---
+++ 01-basics/04-00-variables-types.js
@@ -0,0 +1,96 @@
+// One variable - different types
+// let age = 36
+// console.log(age)
+// age = "Alexander"
+// console.log(age)
+
+// Dynamic assignment of types
+// let a = true
+// var b = 56
+// const myDetails = {"name": "Alex", "age": 36}
+// let number_any;
+
+// string
+// console.log(typeof "John")
+// console.log(typeof 'John')
+// console.log(typeof `John`)
+// console.log(typeof ("John"+"Doe"))
+// const firstName = "Alex"
+// const lastName = "Komanov"
+// console.log(firstName + " " + lastName)
+// console.log(typeof (firstName + " " + lastName))
+
+        
+// number - All JavaScript numbers are stored in a 64-bit floating-point format.
+// console.log(typeof 3.14 )
+// console.log(typeof 33 )
+// console.log(typeof (33 + 66) )
+// console.log(Number.MAX_VALUE)
+// console.log(typeof (Number.MAX_VALUE))
+  
+// boolean
+// console.log(typeof true )
+// console.log(typeof false )
+// console.log(5 > 1)
+// console.log(typeof(5 > 1))
+          
+//  BigInt         
+// console.log(1234n)
+// console.log(typeof 1234n)
+// console.log(BigInt(1234567890123456789012345))
+// console.log(typeof BigInt(1234567890123456789012345))
+// console.log(1234)
+// console.log(typeof 1234)
+// let maxInteger = Number.MAX_SAFE_INTEGER
+// console.log(maxInteger)
+// console.log(maxInteger + 1)
+// console.log(maxInteger + 2)
+// console.log(BigInt(Number.MAX_SAFE_INTEGER))
+// console.log(BigInt(Number.MAX_SAFE_INTEGER) + 2n)
+// console.log(1234567890123456789 * 123)
+// console.log(1234567890123456789n * 123n)
+// console.log(BigInt(Number.MAX_SAFE_INTEGER) + 2)
+
+// Symbol
+// console.log(Symbol())
+// console.log(typeof Symbol())
+// console.log(Symbol("id"))
+// console.log(Symbol("id"))
+// console.log(typeof Symbol("id")) 
+// console.log(Symbol("id") == Symbol("id")) 
+
+// undefined
+// let x;
+// console.log(typeof x)
+// let myName = "text"
+// console.log(typeof myName)
+// myName = undefined
+// console.log(typeof myName)
+
+// null
+// const nullVar = null
+// console.log(nullVar)
+// console.log(typeof nullVar)
+
+// Empty String
+// let myName = ""
+// console.log(myName)
+// console.log(typeof myName)
+
+// String without visible value
+// myName = " "
+// console.log(myName)
+// console.log(typeof myName)
+
+// Weird things
+// console.log((2 + '2')) // '22'
+// console.log(typeof(2 + '2'))
+// console.log(('2' - 2))
+// console.log(typeof('2' - 2))
+// console.log(('alex' - 2))
+// console.log(typeof('alex' - '2'))
+// console.log((2 / 0))
+// console.log(typeof(2 / 0)) 
+// console.log(- (2 / 0))
+// console.log(typeof(2 / 0)) 
+
---
+++ 01-basics/04-01-homework.js
@@ -0,0 +1,38 @@
+// Task 1
+// Use console.log() to print "Hello, World!" to the console.
+// console.log("Hello, World!");
+
+// Create two variables, firstName and lastName, and use console.log() 
+// to print them together in one line.
+// const firstName = "John";
+// const lastName = "Doe";
+// console.log(firstName + " " + lastName);
+
+// Use console.log() to print the result of 5 + 3
+// console.log(5 + 3);
+
+// Task 2
+// Create variables for each primitive type (string, number, boolean, undefined, null) and 
+// print their types using console.log() and the typeof operator.
+// const myString = "Hello";
+// const myNumber = 42;
+// const myBoolean = true;
+// const myUndefined = undefined;
+// const myNull = null;
+
+// console.log(typeof myString);    // "string"
+// console.log(typeof myNumber);    // "number"
+// console.log(typeof myBoolean);   // "boolean"
+// console.log(typeof myUndefined); // "undefined"
+// console.log(typeof myNull);      // "object" 
+
+// Task 3
+//  Fix the following variable declarations to follow proper naming conventions:
+//  const first name = "Alex"
+//  let 1stPlace = "Gold"
+//  var user-age = 25
+
+const firstName = "Alex";
+let firstPlace = "Gold";
+var userAge = 25;
+
