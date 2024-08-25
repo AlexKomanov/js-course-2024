8eef0fff38497fbe94cb6a1ec8ab88545bbfe9d6
AlexKomanov <akomanov88@gmail.com>
2024-08-25 22:23:08 +0300

    Adding home work 07

---
+++ 01-basics/07-03-homework.js
@@ -0,0 +1,44 @@
+/**
+ *  Task 1: Arithmetic Operators
+    Write a JavaScript expression that calculates the average of 13, 27, and 85 using 
+    arithmetic operators.
+ */
+
+// console.log((13 + 27 + 85) / 3)
+
+/**
+ *  Task 2: Assignment Operators
+    Start with a variable x set to 10. Use assignment operators to perform the following 
+    operations in sequence:
+    Add 5 to x
+    Multiply x by 2
+    Subtract 3 from x
+    Divide x by 4
+ */
+
+    // let x = 10 
+    // x += 5 // x is now 15 
+    // x *= 2 // x is now 30 
+    // x -= 3 // x is now 27 
+    // x /= 4 // x is now 6.75 
+    // console.log(x)
+
+
+    /**
+     *  Task 3: Comparison Operators
+        Fix the following incorrect statements so that they match the intended truth value:
+
+        4 >= 5 should be true.
+        4 <= 10 should be false.
+        7 === 3 should be true.
+        12 > 12 should be true.
+        6 !== 6 should be true.
+        8 < 7 should be true.
+     */
+
+        console.log(4 >= 4)
+        console.log(4 <= 3)
+        console.log(7 === 7)
+        console.log(12 > 11)
+        console.log(6 !== '6')
+        console.log(6 < 7)
