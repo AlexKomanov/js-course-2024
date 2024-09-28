2bc02e46c66b51afa243813f8e2af8cdaf1d1bd9
AlexKomanov <akomanov88@gmail.com>
2024-09-28 13:51:41 +0300

    lesson-08-classwork

---
+++ 01-basics/08-01-arrays-basics.js
@@ -0,0 +1,32 @@
+// const singleNumber1 = 10;
+// const singleNumber2 = 20;
+// const numbers = [1, 2, 3, 4, 5]
+// console.log(numbers); // [1, 2, 3, 4, 5]
+
+// const strings = ["hello", "world", "JavaScript"];
+// console.log(strings); // ["hello", "world", "JavaScript"]
+
+// const array2 = new Array(1, 2, 3);
+// console.log(array2); // [1, 2, 3]
+
+// const arr = [1, 2, 3, 4, 5];
+// console.log(arr.length); // 5
+
+// const arr = [10, 20, 30, 40]; // index 0 - 3
+// console.log(arr[0]); // 10
+// console.log(arr[2]); // 30
+// console.log(arr[5]); // out of array length -> undefined
+
+// const arr = [1, 2, 3];
+// console.log(arr); // [1, 2, 3]
+// arr[1] = 20; 
+// arr[2] = 50; 
+// console.log(arr); // [1, 20, 50]
+// arr[5] = 40
+// console.log(arr); // [ 1, 20, 50, <2 empty items>, 40 ]
+
+
+// const mixedArray = [42, "hello", true, { key: "value" }];
+// console.log(mixedArray); // [42, "hello", true, { key: "value" }]
+// console.log(mixedArray[0] * 2); // 84
+// console.log(mixedArray[1] * 2); // NaN
---
+++ 01-basics/08-02-arrays-basic-functions.js
@@ -0,0 +1,61 @@
+
+// const arr = [1, 2, 3];
+// console.log(arr); // [1,2,3]
+// console.log(arr.toString()); // "1,2,3"
+// console.log(arr); // [1,2,3]
+
+// const arr = [1, 2, 3];
+// console.log(arr.join("-*-")); // "1-*-2-*-3"
+// console.log(arr); // [1,2,3]
+
+// const arr = [1, 2, 3];
+// const lastElement = arr.pop();
+// console.log(lastElement); // 3
+// console.log(arr); // [1, 2]
+
+// const arr = [1, 2];
+// console.log(arr.length)
+// const arrLength = arr.push(3);
+
+//  //           index   0  1  2
+// console.log(arr); // [1, 2, 3]
+// console.log(arrLength) // 3
+// arr[3] = 40
+// console.log(arr); // [ 1, 2, 3, 40 ]
+// arr[7] = 80
+// console.log(arr); 
+// arr.push(1, 2, 3, 4);
+// console.log(arr); 
+
+// const arr = [1, 2, 3];
+// const firstElement = arr.shift();
+// console.log(firstElement); // 1
+// console.log(arr); // [2, 3]
+
+// const arr = [2, 3];
+// console.log(arr);
+// const arrLength = arr.unshift(1);
+// console.log(arr); // [1, 2, 3]
+// console.log(arrLength) // 3
+// arr.unshift(-1, 0);
+// console.log(arr); // [ -1, 0, 1, 2, 3 ]
+
+// const arr1 = [1, 2];
+// const arr2 = [3, 4];
+// const newArr = arr1.concat(arr2);
+// console.log(newArr); // [1, 2, 3, 4]
+// console.log(arr1); // [1, 2]
+
+//    index  0  1  2  3  4
+const arr = [1, 2, 3, 4, 5];
+const subArr1 = arr.slice(2);
+const subArr2 = arr.slice(1, 4);
+console.log(subArr1); // [3, 4, 5]
+console.log(subArr2); // [2, 3, 4]
+console.log(arr); // [1, 2, 3, 4, 5]
+
+
+
+
+
+
