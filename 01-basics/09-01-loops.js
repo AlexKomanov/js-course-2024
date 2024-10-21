42f2995256d0ce14eaca09f9af07c26249608255
AlexKomanov <akomanov88@gmail.com>
2024-10-21 19:03:24 +0300

    lesson 09 class part

---
+++ 01-basics/09-01-loops.js
@@ -0,0 +1,62 @@
+// print nubmers from 0 to 5
+// console.log(0);
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+
+// For Loop
+
+// for (let i = 0; i < 5; i++) {
+//     console.log("Before Index");
+//     console.log(i);
+//     console.log("After Index");
+//     console.log("-------------")
+// }
+
+// const numbers = [0, 3, 5, 6, 10]; // length = 5 ; last index = 4
+
+// for (let i = 0; i <= numbers.length; i++) {
+//     console.log(numbers[i]);
+// }
+
+//  For-Of Loop
+
+// const fruits = ['apple', 'banana', 'cherry', 'lemon', "grape"];
+// for (const fruit of fruits) {
+//   console.log(fruit);
+// }
+
+
+// While Loop
+
+// let i = Math.round(Math.random() * 50);
+// while (i >= 0) {
+//     console.log(i);
+//     i--;
+// }
+
+// Operator 'break'
+
+// for (let i = 0; i < 10; i++) {
+//     if (i === 5) {
+//         console.log(i);
+//         console.log("break operator...")
+//         break; 
+//     }
+//     console.log(i);
+// }
+
+// Operator 'continue'
+
+for (let i = 0; i < 10; i++) {
+    if (i === 5) {
+        continue; 
+    }
+    console.log(i);
+}
+
+
+
+
