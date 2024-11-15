40e6e952385d52e5ad3310a4e18806f32be4eb73
AlexKomanov <akomanov88@gmail.com>
2024-11-15 17:21:03 +0200

    Adding Home Work

---
+++ 04-conditions/homework/task-01.js
@@ -0,0 +1,9 @@
+let username = "admin";
+let password = "1234";
+let age = 25;
+
+if (username === "admin" && password === "1234" && age >= 18) {
+    console.log("Access granted!");
+} else {
+    console.log("Access denied!");
+}
---
+++ 04-conditions/homework/task-02.js
@@ -0,0 +1,3 @@
+let currentHour = 15;
+let storeStatus = (currentHour >= 9 && currentHour < 21) ? "Open" : "Closed";
+console.log(storeStatus);
